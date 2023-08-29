f51d79223da0a09babca190e16cdb0513838de5c
seon-mikim <tjsalsun@gmail.com>
2023-08-30 01:17:36 +0900

    conf: 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import DeliveryOrderPage from 'pages/DeliveryOrderPage';
+import './App.css';
+
+
+
+function App() {
+  return <DeliveryOrderPage/>
+  
+}
+
+export default App;
---
+++ src/pages/DeliveryOrderPage.jsx
@@ -0,0 +1,10 @@
+import Header from "@components/Header/Header.jsx"
+
+
+const DeliveryOrderPage = () => {
+	return (
+		<div><Header/></div>
+	)
+}
+
+export default DeliveryOrderPage
