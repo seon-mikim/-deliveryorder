dad7183ccebd1e51113b749fdef7c9dadc84184b
seon-mikim <tjsalsun@gmail.com>
2023-08-30 02:42:54 +0900

    feat: Button 컴포넌트 import 처리
    - Button 컴포넌트 내보내기
    - Button 컴포넌트에 내려줄 props(name, className, onClick) 작성
    - Header 컴포넌트에 받을 프롭스 작성

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import Button from '@components/UI/Button/Button'
 import classes from './Header.module.css'
 
-const Header = () => {
+const Header = ({handleClick }) => {
 	return (
 		<header className={classes['delivery-header'] }>
 			<h1 className={classes['header__text']}>Delivery Order</h1>
+			<div>
+				<Button name='주문하기' className='header__modal-button--active' onClick={handleClick}/>
+			</div>
 		</header>
 	)
 }
