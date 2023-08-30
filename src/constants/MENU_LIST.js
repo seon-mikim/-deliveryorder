399e519deeb9d1c27f9c5919051737fe98504d9a
seon-mikim <tjsalsun@gmail.com>
2023-08-30 21:49:12 +0900

    design:스타일 추가
    - 폰트 굵기, 크기 조절
    - 레이아웃 수정

--- src/constants/MENU_LIST.js
+++ src/constants/MENU_LIST.js
@@ -1,10 +1,10 @@
 const MENU_LIST = [
-  { menuName: '닭발', price: '20000원', amount: '1',  },
-  { menuName: '먹태', price: '12000원', amount: '1' },
-  { menuName: '오돌뼈', price: '15000원', amount: '1' },
-  { menuName: '염통꼬치', price: '5000원', amount: '1' },
-  { menuName: '황도화채', price: '6000원', amount: '1' },
-  { menuName: '파인샤베트', price: '6000원', amount: '1' },
+  { menuName: '닭발', price: 20000, amount: 1 },
+  { menuName: '먹태', price: 12000, amount: 1 },
+  { menuName: '오돌뼈', price: 15000, amount: 1 },
+  { menuName: '염통꼬치', price: 5000, amount: 1 },
+  { menuName: '황도화채', price: 6000, amount: 1 },
+  { menuName: '파인샤베트', price: 6000, amount: 1 },
 ];
 
-export default MENU_LIST
+export default MENU_LIST;
