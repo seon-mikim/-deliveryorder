8cf494748f22f2d84606ebac955377fa1431834c
seon-mikim <tjsalsun@gmail.com>
2023-08-30 22:31:00 +0900

    design: MenuList 컴포넌트 디자인 추가
    - box-shadow 추가
    - margin top 추가
    - background-color 추가

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,9 +1,10 @@
 import Card from '@components/UI/Card/Card';
 import MENU_LIST from 'constants/MENU_LIST';
+import classes from './MenuList.module.css'
 
 const MenuList = () => {
   return (
-    <ul>
+    <ul className={classes['main__menu-list'] }>
       {MENU_LIST.map((menu, index) => (
         <Card key={index} menu={menu} />
       ))}
