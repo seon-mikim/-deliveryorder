9df6f48b8d025d606476c5288ed07119e2b968b7
seon-mikim <tjsalsun@gmail.com>
2023-08-30 16:26:00 +0900

    feat: MENU_LIST constans 생성

---
+++ src/constants/MENU_LIST.js
@@ -0,0 +1,10 @@
+const MENU_LIST = [
+  { menuName: '닭발', price: '20000원', amount: '1' },
+  { menuName: '먹태', price: '12000원', amount: '1' },
+  { menuName: '오돌뼈', price: '15000원', amount: '1' },
+  { menuName: '염통꼬치', price: '5000원', amount: '1' },
+  { menuName: '황도 화채', price: '6000원', amount: '1' },
+  { menuName: '파인 샤베트', price: '6000원', amount: '1' },
+];
+
+export default MENU_LIST
